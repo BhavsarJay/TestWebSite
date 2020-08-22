f19fa74677cbc8901a3bc2627c579ebdd33940dd
BhavsarJay <bhavsarjay01@gmail.com>
2020-08-22 19:16:42 +0530

    Update

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-// This Smooth scrolling is generating some problems.
+//This Smooth scrolling is generating some problems.
+
 // Scrollbar.init(document.querySelector('#my-scrollbar'));
 // var Scrollbar = window.Scrollbar;
 
-
+//For Skrollr
+var s = skrollr.init([easing = 'linear']);
+skrollr.init({
+  smoothScrolling: true,
+  easing: "linear"
+})
 
 var myAnimation = new hoverEffect({
   parent: document.querySelector('.distortion'),
@@ -18,27 +24,41 @@ var myAnimation = new hoverEffect({
   displacementImage: 'https://res.cloudinary.com/therealsk/image/upload/v1593860931/heightMap_jjb5ng.png'
 });
 
-console.log(typeof(myAnimation));
+// console.log(typeof(myAnimation));
 
 var text1 = $(".text-1");
 var text2 = $(".text-2");
-var text3 = $(".text-3");
 var windowHeight = $(window).height();
+var imgIndex = 0;
+var centreOfScreen;
+var scrolledFromtop = 0;
 
 jQuery(window).scroll(function(){
-  var scrolledFromtop = $(window).scrollTop();
-  var centreOfScreen = scrolledFromtop + windowHeight;
+  
+  scrolledFromtop = $(window).scrollTop();
+  centreOfScreen = scrolledFromtop + windowHeight;
 
-
-  if (centreOfScreen > text2.offset().top + text2.height() - 200) {
-    // console.log("CHANGE TO 2nd");
-    myAnimation.next();
+  if (centreOfScreen > text2.offset().top + text2.height()/2) {
+    if(imgIndex != 2){
+      myAnimation.next();
+      imgIndex = 2;
+      console.log("Change To 2nd");
+    }
   }
-  else if(centreOfScreen > text1.offset().top + text1.height() - 200){
-    // console.log("Change To 1st");
-    myAnimation.previous();
+  else if(centreOfScreen > text1.offset().top + text1.height()/2){
+    if(imgIndex != 1){
+      myAnimation.previous();
+      imgIndex = 1;
+      console.log("Change To 1st");
+    }
   }
 
 
-
+  parallaxScroll(scrolledFromtop);
+  // incScale();
 });
+
+function parallaxScroll(scrolled){
+  $('.background-text-items').css('top',(0 + (scrolled * 0.5)) + 'px');
+  $('.bg-texts-container').css('top',(-200 + (scrolled * 0.5)) + 'px');
+}
