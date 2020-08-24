3205c0bdc2175d1e22435fa4859ab6558fad7cf0
BhavsarJay <bhavsarjay01@gmail.com>
2020-08-25 02:33:25 +0530

    Update

--- index.js
+++ index.js
@@ -55,10 +55,153 @@ jQuery(window).scroll(function(){
 
 
   parallaxScroll(scrolledFromtop);
-  // incScale();
 });
 
 function parallaxScroll(scrolled){
   $('.background-text-items').css('top',(0 + (scrolled * 0.5)) + 'px');
   $('.bg-texts-container').css('top',(-200 + (scrolled * 0.5)) + 'px');
+}
+
+var storyImg2 = $('#story-card-2');
+var storyImg1 = $('#story-card-1');
+var storyImg3 = $('#story-card-3');
+// var background = $('#background');
+
+var openStory = -1;
+var imgs = [storyImg1, storyImg2, storyImg3];
+var animProgress = 100;
+
+storyImg1.click(function(){expandItem(0);});
+storyImg2.click(function(){expandItem(1);});
+storyImg3.click(function(){expandItem(2);});
+
+storyImg1.hover(function(){mouseEnter(0);}, function(){mouseLeave(0);});
+storyImg2.hover(function(){mouseEnter(1);}, function(){mouseLeave(1);});
+storyImg3.hover(function(){mouseEnter(2);}, function(){mouseLeave(2);});
+
+
+function expandItem(cardIndex){
+  if(animProgress == 100){
+    if(openStory == -1){
+      openCard(cardIndex);
+    }
+    else if(openStory != cardIndex){
+      closeCard(openStory);
+    }
+  }
+}
+
+function mouseEnter(cardIndex){
+  if(openStory == -1){
+    var opacity_animation = anime({
+      targets: imgs[cardIndex][0].children[0],
+      opacity: ['40%', '70%'],
+      easing: 'linear',
+      direction: 'forwards',
+      duration: 200
+    });
+  }
+}
+
+function mouseLeave(cardIndex){
+  if(openStory == -1){
+    var opacity_animation = anime({
+      targets: imgs[cardIndex][0].children[0],
+      opacity: ['70%', '40%'],
+      easing: 'linear',
+      direction: 'forwards',
+      duration: 200
+    });
+  }
+}
+
+function openCard(cardIndex){
+  var scale_animation = anime({
+    targets: imgs[cardIndex][0],
+    width: '50%',
+    easing: 'easeInOutExpo',
+    delay: 250,
+    update: function(anim){
+      animProgress = anim.progress;
+    }
+  });
+  
+  var backgroundImg = imgs[cardIndex][0].children[0];
+  var opacity_animation = anime({
+    targets: backgroundImg,
+    opacity: ['70%', '100%'],
+    scale: 1.2,
+    easing: 'easeInOutExpo',
+    delay: 0,
+  });
+
+  var headingText = imgs[cardIndex][0].children[1].children[0];
+  var text_animation = anime({
+    targets: headingText,
+    top: 60,
+    color: '#000',
+    easing: 'easeInOutExpo',
+    // delay: 250
+  });
+
+  var para = imgs[cardIndex][0].children[1].children[1];
+  var para_animation = anime({
+    targets: para,
+    opacity: [0, 1],
+    top: 180,
+    easing: 'easeInOutExpo',
+    delay: 250,
+    begin: function(anim){
+      para.style.display = "initial";
+    }
+  });
+
+  console.log('play');
+  openStory = cardIndex;
+}
+
+function closeCard(cardIndex){
+  var scale_animation = anime({
+    targets: imgs[cardIndex][0],
+    width: '25%',
+    easing: 'easeInOutExpo',
+    delay: 0,
+    update: function(anim){
+      animProgress = anim.progress;
+    },
+  });
+
+  var backgroundImg = imgs[cardIndex][0].children[0];
+  var opacity_animation = anime({
+    targets: backgroundImg,
+    opacity: ['100%', '70%'],
+    scale: 1,
+    easing: 'easeInOutExpo',
+    delay: 250,
+  });
+
+  var headingText = imgs[cardIndex][0].children[1].children[0];
+  var text_animation = anime({
+    targets: headingText,
+    top: '200',
+    color: '#FFF',
+    easing: 'easeInOutExpo',
+    delay: 250
+  });
+
+  var para = imgs[cardIndex][0].children[1].children[1];
+  var para_animation = anime({
+    targets: para,
+    opacity: [1, 0],
+    top: 200,
+    easing: 'easeInOutExpo',
+    // delay: 250
+    complete: function(anim){
+      para.style.display = "none";
+      mouseLeave(cardIndex);
+    }
+  });
+
+  console.log('reverse');
+  openStory = -1;
 }
